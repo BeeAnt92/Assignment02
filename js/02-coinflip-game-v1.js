b1513ac3b8881a5bc1b43995ba60e28a0de980f6
Brian J <Brian.jones92@gmail.com>
2026-06-24 14:56:25 -0700

    CoinFlipv1 Done

--- js/02-coinflip-game-v1.js
+++ js/02-coinflip-game-v1.js
@@ -1,20 +1,18 @@
-let coinFlip = randomNum
 let randomNum = Math.round(Math.random())
+let coinFlip = randomNum
 let choice = prompt('Heads or Tails?')
+let result;
 if (choice === 'Heads') {
-    if (randomNum <= coinFlip) {
-        alert('The flip was heads and you chose heads...you win!')
-    } 
-} else if (choice === 'Tails') {
-    if (randomNum >= coinFlip) {
-        alert('The flip was heads and you chose tails...you lose!')
-    }
-} else if (choice === 'Heads') {
-    if (randomNum >= coinFlip) {
-        alert('The flip was tails and you chose heads...you lose!')
+    if (randomNum <= 0) {
+        result = ('The flip was heads and you chose heads...you win!')
+    } else if (randomNum >= 1) {
+        result = ('The flip was tails and you chose heads...you lose!')
     }
 } else if (choice === 'Tails') {
-    if (randomNum <= coinFlip) {
-        alert('The flip was tails and you chose tails...you win!')
+    if (randomNum <= 0) {
+        result = ('The flip was heads and you chose tails...you lose!')
+    } else if (randomNum >= 1) {
+        result = ('The flip was tails and you chose tails...you win!')
     }
 }
+alert(result)
