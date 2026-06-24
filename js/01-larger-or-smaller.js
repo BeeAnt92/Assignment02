7f3dede2adaa923a4bad472cae676a470396262f
Brian J <Brian.jones92@gmail.com>
2026-06-24 12:27:33 -0700

    Assignment02

---
+++ js/01-larger-or-smaller.js
@@ -0,0 +1,8 @@
+let num1 = prompt('Enter a number between 1 and 10')
+let num2 = prompt('Enter another number between 1 and 10')
+if (num1 >= num2) {
+    alert(`${num1} is larger than ${num2}`)
+} else if ( num2 >= num1) {
+    alert(`${num2} is larger than ${num1}`)
+}
+
