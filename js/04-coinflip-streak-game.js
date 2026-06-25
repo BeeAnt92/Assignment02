5dae6f434cabd646f69568b6dc25a7f0d1085ee9
Brian J <Brian.jones92@gmail.com>
2026-06-24 17:12:57 -0700

    CoinFlipStreakGameDone

---
+++ js/04-coinflip-streak-game.js
@@ -0,0 +1,14 @@
+let coinFlip;
+let randomNum = Math.round(Math.random());
+let result;
+
+do {
+    coinFlip = randomNum > 20 
+    if (coinFlip === 0) {
+        console.log('Heads')
+    } else (coinFlip === 1); {
+        console.log('Tails')
+    }
+    result = coinFlip
+} while (result === 1) {
+alert('Thank you for playing!')}
